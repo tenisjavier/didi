a2e866682ae251197fd33e64c5adc9edba60be47
Javier Borquez <javierborquez@didiglobal.com>
2022-02-08 21:40:25 -0300

    fix link without react router

--- src/components/sections/GuidesColumns.js
+++ src/components/sections/GuidesColumns.js
@@ -3,11 +3,8 @@ import { Link } from "gatsby";
 import ColumnsSection from "../ColumnSection";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import slugify from "react-slugify";
-import { useLocation } from "@reach/router";
 
 const GuidesColumns = ({ data }) => {
-  const { protocol, host } = useLocation();
-  console.log(useLocation());
   const guides = data.allContentfulGuide.nodes;
   const bgColor = "bg-blue-primary";
   const title = "Guías para Socios Conductores";
@@ -16,7 +13,7 @@ const GuidesColumns = ({ data }) => {
   let columns = [];
   guides.forEach((guide) => {
     const slug = slugify(guide.title);
-    const link = `${protocol}//${host}/cl/guias/${slug}`;
+    const link = `/cl/guias/${slug}`;
     console.log(link);
     columns.push({
       title: <Link to={link}>{guide.title}</Link>,
@@ -24,13 +21,15 @@ const GuidesColumns = ({ data }) => {
       textColor: "gray-primary",
       bgColor: "bg-white",
       image: (
-        <GatsbyImage
-          image={getImage(guide.featuredImage)}
-          alt={guide.title}
-          width={700}
-          height={700}
-          className="z-10  m-4"
-        ></GatsbyImage>
+        <Link to={link}>
+          <GatsbyImage
+            image={getImage(guide.featuredImage)}
+            alt={guide.title}
+            width={700}
+            height={700}
+            className="z-10  m-4"
+          ></GatsbyImage>
+        </Link>
       ),
       btnText: "Leer Guía",
       btnLink: link,
