2ef807eaaf41f07f49b848b8ec35aaab72d722b8
Javier Borquez <javierborquez@didiglobal.com>
2022-02-11 13:45:25 -0300

    breadcrumb

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,21 @@
 import React from "react";
 import { Link } from "gatsby";
 import { StaticImage } from "gatsby-plugin-image";
+import Breadcrumb from "./Breadcrumb";
+
 import Menu from "./Menu";
 
 const Header = (props) => {
   return (
-    <nav className="bg-gray-primary fixed z-20 h-20 w-full bg-opacity-80">
-      <div className="flex h-full items-center justify-between">
-        <Navlogo />
-        <Menu></Menu>
-      </div>
-    </nav>
+    <>
+      <nav className="bg-gray-primary fixed z-20 h-20 w-full bg-opacity-80">
+        <div className="flex h-full items-center justify-between">
+          <Navlogo />
+          <Menu></Menu>
+        </div>
+      </nav>
+      <Breadcrumb></Breadcrumb>
+    </>
   );
 };
 
