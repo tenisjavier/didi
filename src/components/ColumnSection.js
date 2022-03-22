4dc832814b051e00842404174a40eb934465300e
Javier Borquez <javierborquez@didiglobal.com>
2022-03-21 22:24:45 -0300

    fix error and rancagua

--- src/components/ColumnSection.js
+++ src/components/ColumnSection.js
@@ -8,9 +8,7 @@ const ColumnsSection = (props) => {
       <div className="container mx-auto flex  flex-col flex-wrap justify-center">
         {title && <h2 className="text-center text-3xl">{title}</h2>}
         {desc && <p className="text-center">{desc}</p>}
-        <div
-          className={`border-1 border-red mt-10 flex flex-wrap justify-around `}
-        >
+        <div className={`mt-10 flex flex-wrap justify-around `}>
           {columns.map((col, index) => {
             return <Card {...col} key={index}></Card>;
           })}
