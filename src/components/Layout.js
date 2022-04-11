b3091070718ce20c5815429ade4b2b69db94fb9a
Javier Borquez <javierborquez@Javiers-MacBook-Pro.local>
2022-04-10 21:43:24 -0400

    gtm - static folder with gtm head

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,17 +9,6 @@ import "../styles/global.css";
 const Layout = ({ children }) => {
   return (
     <>
-      {
-        // <noscript>
-        //   <iframe
-        //     src="https://www.googletagmanager.com/ns.html?id=GTM-PKNGZ5P"
-        //     height="0"
-        //     width="0"
-        //     className="hidden"
-        //     title="GTM"
-        //   ></iframe>
-        // </noscript>
-      }
       <Seo></Seo>
       <Header></Header>
       {children}
--- src/components/SEO.js
+++ src/components/SEO.js
@@ -23,6 +23,7 @@ const SEO = () => {
       htmlAttributes={{ lang: "es" }}
       title={`${meta.title} | DiDi ${country}`}
     >
+      <script src="/gtm.js" type="text/javascript" />
       <meta name="description" content={meta.desc} />
       <link rel="canonical" href={host + pathname} />
       <link rel="alternate" href={host + pathname} hreflang="es-CL" />
@@ -31,20 +32,6 @@ const SEO = () => {
         useEffect(() => {
           insertBtnParams();
         }, [])
-      }
-
-      {
-        // //Google Tag Manager
-        // (function (w, d, s, l, i) {
-        //   w[l] = w[l] || [];
-        //   w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
-        //   var f = d.getElementsByTagName(s)[0],
-        //     j = d.createElement(s),
-        //     dl = l !== "dataLayer" ? "&l=" + l : "";
-        //   j.async = true;
-        //   j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
-        //   f.parentNode.insertBefore(j, f);
-        // })(window, document, "script", "dataLayer", "GTM-PKNGZ5P")
       }
     </Helmet>
   );
