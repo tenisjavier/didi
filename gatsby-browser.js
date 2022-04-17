ae4bb3571de80febdd32d8462ce240b3d14a58ea
Javier Borquez <javierborquez@Javiers-MacBook-Pro.local>
2022-04-17 12:39:22 -0400

    fix - Layout browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
 // import "@popperjs/core/dist/umd/popper.min";
 // import "./src/styles/global.css";
 const React = require("react");
-const Layout = require("./src/components/layout").default;
+const Layout = require("./src/components/Layout").default;
 
 exports.wrapPageElement = ({ element, props }) => {
   // props provide same data to Layout as Page element will get
