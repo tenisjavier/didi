864bac7092c6142345bcd342418cefaaffbed64a
tenisjavier <tenis.javier@gmail.com>
2022-07-20 12:05:29 -0400

    pa - slider conductor

--- src/pages/do/driver/index.js
+++ src/pages/do/driver/index.js
@@ -5,6 +5,7 @@ import DrvHero from "../../../components/sections/DrvHero";
 import DrvWhyDiDi from "../../../components/sections/DrvWhyDiDi";
 import DrvCityList from "../../../components/sections/DrvCityList";
 import HomeColumns from "../../../components/sections/PaxColumns";
+import SilderSection from "../../../components/sections/SliderSection";
 import Requirements from "../../../components/sections/Requirements";
 import KnowMoreBanner from "../../../components/sections/KnowMoreBanner";
 
@@ -23,6 +24,10 @@ const Driver = ({ data }) => {
     <Layout>
       <DrvHero bgImage={drvHeroBgImage}></DrvHero>
       <DrvWhyDiDi image={drvWhyDiDiImage}></DrvWhyDiDi>
+      <SilderSection
+        data={products}
+        title="Hay un DiDi para ti"
+      ></SilderSection>
       <Requirements data={products}></Requirements>
       <KnowMoreBanner></KnowMoreBanner>
       <HomeColumns></HomeColumns>
