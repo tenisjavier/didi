048914dbb3c12879dc50a3b85f34a43d2f50c63b
tenisjavier <tenis.javier@gmail.com>
2022-08-12 12:14:03 -0400

    int - Home About Us and Missions

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { GatsbyImage } from "gatsby-plugin-image";
 import Seo from "../components/SEO";
 import { Link, graphql } from "gatsby";
 import { StaticImage } from "gatsby-plugin-image";
@@ -7,10 +8,17 @@ import HeroVideo from "../images/didi-home.mp4";
 
 const Index = ({ data }) => {
   const countries = data.allContentfulCountry.nodes;
+  const images = data.allContentfulAsset.nodes;
+  const HomeAboutUsImage = images.filter((image) => {
+    return image.title === "int.HomeAboutUs.image";
+  })[0];
+  const HomeOurMissionImage = images.filter((image) => {
+    return image.title === "int.HomeOurMission.image";
+  })[0];
   const CTAProps = {
     hero: true,
     title: "Welcome to DiDi.",
-    desc: "More than a journey.",
+    desc: "More than a Journey. The World‘s Leading Transportation Platform.",
     textColor: "white",
     bgColor: "bg-gray-primary",
     bgImage: (
@@ -35,6 +43,42 @@ const Index = ({ data }) => {
     alignItems: "center",
   };
 
+  const AboutUsProps = {
+    hero: true,
+    title: "About Us",
+    desc: "DiDi Global Inc. is the world’s leading mobility technology platform. We offer a wide range of app-based services across markets including Asia-Pacific, Latin America, Europe and Africa, including ride hailing, taxi hailing, chauffeur, hitch and other forms of shared mobility as well as auto solutions, food delivery, intra-city freight and financial services. DiDi provides car owners, drivers and delivery partners with flexible work and income opportunities. We are committed to collaborating with policymakers, the taxi industry, the automobile industry and local communities to solve the world’s transportation, environmental and employment challenges through the use of AI technology and localized smart transportation innovations. DiDi strives to create better life experiences and greater social value by building a safe, inclusive and sustainable transportation and local services ecosystem for cities of the future.",
+    textColor: "gray-primary",
+    bgColor: "bg-white",
+    image: (
+      <GatsbyImage
+        image={HomeAboutUsImage.gatsbyImageData}
+        alt={HomeAboutUsImage.description}
+        className="z-10 m-4 w-100 rounded"
+      ></GatsbyImage>
+    ),
+  };
+
+  const MissionProps = {
+    hero: true,
+    title: "Mission: To build a better journey. Vision, to become:",
+    bullets: [
+      "The world’s largest one-stop transportation platform",
+      "The world’s largest operator of vehicle networks",
+      "A global leader in smart transportation technologies",
+      "A global leader in the revolution in transportation and automotive technology",
+    ],
+    textColor: "gray-primary",
+    bgColor: "bg-gray-light",
+    image: (
+      <GatsbyImage
+        image={HomeOurMissionImage.gatsbyImageData}
+        alt={HomeOurMissionImage.description}
+        className="z-10 m-4 w-100 rounded"
+      ></GatsbyImage>
+    ),
+    reverse: true,
+  };
+
   return (
     <>
       <Seo></Seo>
@@ -51,6 +95,8 @@ const Index = ({ data }) => {
         </div>
       </nav>
       <CTASection {...CTAProps}></CTASection>
+      <CTASection {...AboutUsProps}></CTASection>
+      <CTASection {...MissionProps}></CTASection>
       <footer>
         <div className="h-96 bg-gray-primary lg:h-80">
           <div className="container mx-auto flex h-full flex-wrap">
@@ -116,6 +162,18 @@ export const query = graphql`
       nodes {
         name
         hostname
+      }
+    }
+    allContentfulAsset(
+      filter: {
+        title: { in: ["int.HomeAboutUs.image", "int.HomeOurMission.image"] }
+      }
+    ) {
+      nodes {
+        id
+        title
+        description
+        gatsbyImageData
       }
     }
   }
