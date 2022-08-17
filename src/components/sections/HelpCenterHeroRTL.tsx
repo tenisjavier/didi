ed54b9bf97dd551ba4d5519d942fc86945444e04
Ramon Mattos <ramon.mattos@brave.ag>
2022-08-17 15:45:08 -0300

    Create HelpCenterHeroRTL.tsx

---
+++ src/components/sections/HelpCenterHeroRTL.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { GatsbyImage, IGatsbyImageData } from "gatsby-plugin-image";
+import { useTranslation } from "gatsby-plugin-react-i18next";
+import CTASection, { CTAProps } from "../CTASectionRTL";
+
+interface HelpCenterHeroProps {
+  bgImage: {
+    title: string;
+    description: string;
+    gatsbyImageData: IGatsbyImageData;
+  };
+}
+
+const HelpCenterHeroRTL = ({ bgImage }: HelpCenterHeroProps) => {
+  const { t } = useTranslation();
+  const props: CTAProps = {
+    hero: true,
+    title: t("HelpCenterHeroRTL.title"),
+    desc: t("HelpCenterHeroRTL.desc"),
+    textColor: t("HelpCenterHeroRTL.textColor"),
+    bgImage: (
+      <GatsbyImage
+        image={bgImage.gatsbyImageData}
+        alt={bgImage.description}
+        className="!absolute z-0 h-full w-full brightness-75 md:block"
+      ></GatsbyImage>
+    ),
+    btnMode: t("HelpCenterHeroRTL.btnMode"),
+    btnType: "both",
+    reverse: true,
+  };
+  return <CTASection {...props}></CTASection>;
+};
+
+export default HelpCenterHeroRTL;
