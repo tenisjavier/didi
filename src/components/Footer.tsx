cba47a202cf0b79f4576dfa0a5721003e2e09a92
Ramon Mattos <ramon.mattos@brave.ag>
2022-08-23 10:44:23 -0300

    [Egypt] Footer country links in arabic + breadcrumb in arabic

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -16,6 +16,9 @@ const Footer = () => {
       allContentfulCountry {
         nodes {
           name
+          arabicName
+          englishName
+          spanishName
           hostname
         }
       }
@@ -25,7 +28,7 @@ const Footer = () => {
   const { i18n } = useTranslation();
   const countryCode = i18n.language;
   const links = getFooterLinks(countryCode);
-
+  
   return (
     <footer>
       <div className="border-buffer h-32 border-x-0 border-b-2 border-t-0 border-solid border-white bg-gray-primary text-white ">
@@ -47,29 +50,56 @@ const Footer = () => {
 
             <div className="text-c h-auto w-3/4 lg:w-full lg:pr-52 lg:text-left">
               {countries.map((c: any, index: number) => {
-                if (index === 0) {
-                  return (
-                    <a
-                      href={c.hostname}
-                      key={index}
-                      className="text-sm text-yellow-500 hover:text-yellow-300"
-                    >
-                      {c.name}
-                    </a>
-                  );
-                } else {
-                  return (
-                    <span key={index} className="text-white">
-                      {" "}
-                      •{" "}
+                if(i18n.language == 'eg') {
+                  if (index === 0) {
+                    return (
                       <a
                         href={c.hostname}
+                        key={index}
+                        className="text-sm text-yellow-500 hover:text-yellow-300"
+                      >
+                        {c.arabicName}
+                      </a>
+                    );
+                  } else {
+                    return (
+                      <span key={index} className="text-white">
+                        {" "}
+                        •{" "}
+                        <a
+                          href={c.hostname}
+                          className="text-sm text-yellow-500 hover:text-yellow-300"
+                        >
+                          {c.arabicName}
+                        </a>
+                      </span>
+                    );
+                  }
+                } else {
+                  if (index === 0) {
+                    return (
+                      <a
+                        href={c.hostname}
+                        key={index}
                         className="text-sm text-yellow-500 hover:text-yellow-300"
                       >
                         {c.name}
                       </a>
-                    </span>
-                  );
+                    );
+                  } else {
+                    return (
+                      <span key={index} className="text-white">
+                        {" "}
+                        •{" "}
+                        <a
+                          href={c.hostname}
+                          className="text-sm text-yellow-500 hover:text-yellow-300"
+                        >
+                          {c.name}
+                        </a>
+                      </span>
+                    );
+                  }
                 }
               })}
             </div>
