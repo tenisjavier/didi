5231dc88289b9f25d897014ed3946f9cfe921167
tenisjavier <tenis.javier@gmail.com>
2022-09-07 14:41:30 -0400

    au - driver safety

--- src/pages/ar/taxi.js
+++ src/pages/ar/taxi.js
@@ -29,7 +29,9 @@ const Taxi = ({ data }) => {
 
 export const query = graphql`
   query ($language: String!) {
-    locales: allLocale(filter: { language: { eq: $language } }) {
+    locales: allLocale(
+      filter: { ns: { in: ["translation"] }, language: { eq: $language } }
+    ) {
       edges {
         node {
           ns
