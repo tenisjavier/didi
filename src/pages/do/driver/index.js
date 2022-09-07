de0d4d86e902b828a41dc2bfb275f67283b381ba
tenisjavier <tenis.javier@gmail.com>
2022-09-07 12:46:40 -0400

    int - fix ns driver

--- src/pages/do/driver/index.js
+++ src/pages/do/driver/index.js
@@ -40,7 +40,9 @@ export default Driver;
 
 export const query = graphql`
   query ($language: String!) {
-    locales: allLocale(filter: { language: { eq: $language } }) {
+    locales: allLocale(
+      filter: { ns: { in: ["translation"] }, language: { eq: $language } }
+    ) {
       edges {
         node {
           ns
