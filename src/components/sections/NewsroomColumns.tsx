fc050f5d4c2d37655d65ecd2a63a031c94763fa0
tenisjavier <tenis.javier@gmail.com>
2022-09-22 14:21:39 -0300

    int - test ruta de la buena onda

---
+++ src/components/sections/NewsroomColumns.tsx
@@ -0,0 +1,59 @@
+// @desc Article Grid used for navigation
+import React from "react";
+import { Link } from "gatsby";
+import { GatsbyImage, getImage, ImageDataLike } from "gatsby-plugin-image";
+import { useTranslation } from "gatsby-plugin-react-i18next";
+import ColumnsSection, { ColumnsSectionProps } from "../ColumnSection";
+
+interface NewsroomColumnsProps {
+  data: {
+    allContentfulArticle: {
+      nodes: {
+        title: string;
+        slug: string;
+        excerpt: string;
+        featuredImage: ImageDataLike;
+      }[];
+    };
+  };
+}
+
+const ArticlesColumns = ({ data }: NewsroomColumnsProps) => {
+  const { t } = useTranslation();
+  const props: ColumnsSectionProps = {
+    title: t("NewsroomColumns.title"),
+    bgColor: t("NewsroomColumns.bgColor"),
+    textColor: t("NewsroomColumns.textColor"),
+    columns: [],
+  };
+
+  const articles = data.allContentfulArticle.nodes;
+
+  props.columns = articles.map((article) => {
+    const link = t("NewsroomColumns.linkItem", {
+      article: article.slug,
+    });
+    return {
+      title: <Link to={link}>{article.title}</Link>,
+      desc: article.excerpt,
+      textColor: "gray-primary",
+      bgColor: "bg-white",
+      image: (
+        <Link to={link}>
+          <GatsbyImage
+            image={getImage(article.featuredImage)!}
+            alt={article.title}
+            className="z-10  m-4"
+          ></GatsbyImage>
+        </Link>
+      ),
+      btnText: t("NewsroomColumns.btnText"),
+      btnLink: link,
+      btnMode: t("NewsroomColumns.btnMode"),
+      height: "h-96",
+    };
+  });
+  return <ColumnsSection {...props}></ColumnsSection>;
+};
+
+export default ArticlesColumns;
