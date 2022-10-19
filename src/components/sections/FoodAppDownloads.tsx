b1dfa6f655d69023496156306dac393abef46a88
Cassiano <cassiano.thurow@gmail.com>
2022-10-19 14:01:13 -0300

    add: download section on delegaciones

---
+++ src/components/sections/FoodAppDownloads.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { GatsbyImage, IGatsbyImageData } from "gatsby-plugin-image";
+import { useTranslation } from "gatsby-plugin-react-i18next";
+import ColumnsSection, { ColumnsSectionProps } from "../ColumnSection";
+
+interface FoodAppDownloadsProps {
+  images: {
+    title: string;
+    description: string;
+    gatsbyImageData: IGatsbyImageData;
+  }[];
+}
+
+const FoodAppDownloads = ({ images }: FoodAppDownloadsProps) => {
+  const { t } = useTranslation();
+  const props: ColumnsSectionProps = {
+    title: t("FoodAppDownloads.title"),
+    desc: t("FoodAppDownloads.desc"),
+    bgColor: t("FoodAppDownloads.bgColor"),
+    textColor: t("FoodAppDownloads.textColor"),
+    columns: t("FoodAppDownloads.columns", { returnObjects: true }),
+  };
+
+  props.columns.forEach((col: any, index: number) => {
+    const image = images[index].gatsbyImageData;
+    col.image = (
+      <a href={col.linkItem}>
+        <GatsbyImage
+          image={image}
+          alt={images[index].description}
+          className="z-10"
+        ></GatsbyImage>
+      </a>
+    );
+  });
+
+  return <ColumnsSection {...props}></ColumnsSection>;
+};
+
+export default FoodAppDownloads;
