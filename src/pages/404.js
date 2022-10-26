5d21b913bed4a09f28361792ef8581a49faf677a
tenisjavier <tenis.javier@gmail.com>
2022-10-26 11:17:18 -0300

    mx - hide business pay

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,15 @@
 // 404 page will redirect to the home page
 import React from "react";
 import { Link } from "gatsby";
+import { Layout } from "../components/Layout";
 
 export default function NotFoundPage() {
   return (
-    <div>
+    <Layout sb={false}>
       <h1>
-        Page Not Found. Visit our Home Page{" "}
-        <Link to="https://web.didiglobal.com"></Link>
+        Page Not Found. Visit our
+        <a href="https://web.didiglobal.com">Home Page</a>
       </h1>
-    </div>
+    </Layout>
   );
 }
