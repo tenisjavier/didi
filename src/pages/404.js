634f0dd56e0da94014018774b4d8c0a0e066d2f0
tenisjavier <tenis.javier@gmail.com>
2022-10-26 11:58:44 -0300

    mx - 404 fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,6 @@
 // 404 page will redirect to the home page
 import React from "react";
-import { Link } from "gatsby";
-import { Layout } from "../components/Layout";
 
 export default function NotFoundPage() {
-  return (
-    <Layout sb={false}>
-      <h1>
-        Page Not Found. Visit our
-        <a href="https://web.didiglobal.com">Home Page</a>
-      </h1>
-    </Layout>
-  );
+  return <h1>Page Not Found. Visit our Home Page</h1>;
 }
