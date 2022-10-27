b8888bc9fbc29dbb4eccf0f4b23e5405b9a606f1
tenisjavier <tenis.javier@gmail.com>
2022-10-27 11:29:36 -0300

    int - drv hero img fix

---
+++ src/pages/popup.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+
+const popup = () => {
+  //   const storageType = localStorage;
+  //   const consentName = "didi_consent";
+
+  //   const shouldShowPopup = () => !storageType.getItem(consentName);
+  //   const saveConsent = () => storageType.setItem(consentName, true);
+
+  //   useEffect(() => {
+  // if(!shouldShowPopup) return;
+  // const consent = confirm('Cookies?')
+  // if(consent) saveConsent()
+  //   }, [])
+
+  return <div>popup</div>;
+};
+
+export default popup;
