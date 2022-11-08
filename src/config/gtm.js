781c8a677c03c5bcd93d41929a0fdb5a628f31f5
tenisjavier <tenis.javier@gmail.com>
2022-11-08 11:59:29 -0300

    gtm event click btn

--- src/config/gtm.js
+++
@@ -1,9 +0,0 @@
-// codigo de tracking didi SEO y WEB
-const gtmEvent = (eventName, params) => {
-  window.dataLayer.push({
-    event: eventName,
-    ...params,
-  });
-};
-
-export default gtmEvent;
