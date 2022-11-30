d3e62d0a2806165f12593cce83887a3d850937b3
tenisjavier <tenis.javier@gmail.com>
2022-11-30 11:46:44 -0300

    ar - store fleet

---
+++ src/pages/ar/store-fleet.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import Layout from "../../components/Layout";
+import StoreFleetHero from "../../components/sections/StoreFleetHero";
+import { QRCodeSVG } from "qrcode.react";
+
+const Store = () => {
+  const [QRUrl, setQRUrl] = useState(
+    "http://fleet.onelink.me/tLtr/fleetmacregion"
+  );
+  const qr = (
+    <QRCodeSVG
+      value={QRUrl}
+      size={300}
+      height={300}
+      width={300}
+      bgColor="#ffffff"
+      className="z-10 m-4 w-100"
+    ></QRCodeSVG>
+  );
+
+  useEffect(() => {
+    setQRUrl(
+      document
+        .getElementsByClassName("btn-light")[0]
+        .getElementsByTagName("a")[0].href
+    );
+  }, []);
+
+  return (
+    <Layout>
+      <StoreFleetHero image={qr}></StoreFleetHero>
+    </Layout>
+  );
+};
+
+export default Store;
