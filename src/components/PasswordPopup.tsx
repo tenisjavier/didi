5604f83dcb00095194d5f37553446981675c00af
tenisjavier <tenis.javier@gmail.com>
2022-12-01 23:46:27 -0300

    int - password design

--- src/components/PasswordPopup.tsx
+++ src/components/PasswordPopup.tsx
@@ -14,18 +14,23 @@ const PasswordPopup = ({ handleShowPassword }: PasswordPopupProps) => {
 
   return (
     <section
-      className={`w-full h-full p-0 m-0 fixed z-30 bg-orange-primary text-white items-center left-0 bottom-0 flex justify-center`}
+      className={`w-full h-full p-0 m-0 fixed z-30 bg-gray-light text-white items-center grid justify-center`}
     >
-      <div className="bg-gray-primary rounded shadow-md p-8 relative left-1/4">
+      <div className="bg-gray-primary rounded shadow-md p-10">
         <h2 className="text-xl text-left">Protected Site</h2>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className="flex flex-col">
           <input
-            type="text"
+            className="shadow appearance-none border rounded w-full py-2 px-5 text-gray-700 leading-tight focus:outline-none focus:shadow-outline my-5 "
+            type="password"
             placeholder="password"
             value={pass}
             onChange={(e) => setPass(e.target.value)}
           />
-          <input type="submit" value="Submit" />
+          <input
+            type="submit"
+            value="Submit"
+            className="bg-blue-primary hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline"
+          />
         </form>
       </div>
       <div className="text-right w-full"></div>
