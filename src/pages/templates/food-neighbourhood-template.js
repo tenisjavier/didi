5dc7c3dd3441e95d175db81a330f3835a42efc28
tenisjavier <tenis.javier@gmail.com>
2023-02-09 11:51:08 -0300

    mx - food template urls

--- src/pages/templates/food-neighbourhood-template.js
+++ src/pages/templates/food-neighbourhood-template.js
@@ -11,32 +11,14 @@ import FoodAppDownloads from "../../components/sections/FoodAppDownloads";
 
 const FoodCity = ({ data }) => {
   const images = data.allContentfulAsset.nodes;
-  const itemsList = data.allContentfulNeighbourhood.nodes.name
-    ? data.allContentfulNeighbourhood.nodes.municipality.neighbourhood.name
+  const cities = data.contentfulNeighbourhood.municipality
+    ? data.contentfulNeighbourhood.municipality.neighbourhood
     : [];
-  const name = data.contentfulNeighbourhood.name;
+  const cityName = data.contentfulNeighbourhood.city.name;
+  const citySlug = data.contentfulNeighbourhood.city.slug;
   const municipalityName = data.contentfulNeighbourhood.municipality.name;
-  const cityName = data.contentfulNeighbourhood.municipality.city.name;
-  const citySlug = data.contentfulNeighbourhood.municipality.city.slug;
   const municipalitySlug = data.contentfulNeighbourhood.municipality.slug;
-  const customBreadcrumb = [
-    {
-      link: `https://web.didiglobal.com/mx/food/`, 
-      text: "Food"
-    },
-    {
-      link: `https://web.didiglobal.com/mx/food/${citySlug}`,
-      text: cityName
-    },
-    {
-      link: `https://web.didiglobal.com/mx/food/${citySlug}/${municipalitySlug}`,
-      text: municipalityName
-    },
-    {
-      link: "#",
-      text: name
-    }
-  ]
+  const name = data.contentfulNeighbourhood.name;
 
   const foodHeroBgImage = images.filter((image) => {
     return image.title === "mx.FoodHero.bgImage";
@@ -54,6 +36,19 @@ const FoodCity = ({ data }) => {
     return image.title.indexOf("mx.FoodDeliveryDownloads.image") !== -1;
   });
 
+  //! Is not a good solution needs refactor to be more flexible in other countries
+  const customBreadcrumb = [
+    { link: `https://web.didiglobal.com/mx/food/`, text: "Food" },
+    {
+      link: `https://web.didiglobal.com/mx/food/ciudad/${citySlug}/`,
+      text: cityName,
+    },
+    {
+      link: `https://web.didiglobal.com/mx/food/colonia/${municipalitySlug}/`,
+      text: municipalityName,
+    },
+    { link: "#", text: name },
+  ];
   return (
     <Layout
       title={`Pide Comida a Domicilio  en ${name} CDMX`}
@@ -76,7 +71,7 @@ const FoodCity = ({ data }) => {
         data={data.contentfulNeighbourhood}
         image={foodDeliveryCTAImage}
       ></FoodCityBannerCTA2>
-      <FoodNeighborhoodList data={itemsList}></FoodNeighborhoodList>
+      <FoodNeighborhoodList data={cities}></FoodNeighborhoodList>
       <FoodCityBannerCTA3
         data={data.contentfulNeighbourhood}
         image={foodCTA3Image}
@@ -90,45 +85,19 @@ export default FoodCity;
 
 export const query = graphql`
   query ($id: String) {
-    allContentfulNeighbourhood (filter: {id: {ne: $id}, city: {country: {code: {eq: "mx"}}}}) {
-    nodes {
-      name
-      municipality {
-        name
-        slug
-        neighbourhood {
-          name
-        }
-        city {
-          name
-          slug
-        }
-      }
-      city {
-        slug
-        name
-      }
-    }
-  }
     contentfulNeighbourhood(id: { eq: $id }) {
       name
       slug
       city {
-        slug
         name
+        slug
       }
       municipality {
-        slug
         name
+        slug
         neighbourhood {
           name
-        }
-        city{
           slug
-          name
-          country {
-            code
-          }
         }
       }
     }
