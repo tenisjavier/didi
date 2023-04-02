b80ea9a9ef94d884e6c6a489486b7e65f1a6442d
tenisjavier <tenis.javier@gmail.com>
2023-04-02 07:05:03 -0400

    fix ab try2

--- src/pages/mx/home.js
+++ src/pages/mx/home.js
@@ -8,7 +8,7 @@ import FoodCTA from "../../components/sections/FoodCTA";
 import FleetAboutCTA from "../../components/sections/FleetAboutCTA";
 import PaxCTA from "../../components/sections/PaxCTA";
 
-//? variant b home
+//? variant b
 const Index = ({ data }) => {
   const images = data.allContentfulAsset.nodes;
   const homeHeroBgImage = images.filter((image) => {
