a5fb9cba0272593f2f21c4ebaefa986061ef308b
Murilo Uarth <murastuarth@gmail.com>
2023-06-01 17:28:08 -0300

    VehicleRequirements section back on mx

--- src/pages/mx/conductor/requisitos-para-conducir.js
+++ src/pages/mx/conductor/requisitos-para-conducir.js
@@ -3,6 +3,7 @@ import { graphql } from "gatsby";
 import Layout from "../../../components/Layout";
 import RequirementHero from "../../../components/sections/RequirementHero";
 import DrvRequirementColumns from "../../../components/sections/DrvRequirementColumns";
+import VehicleRequirementCTA from "../../../components/sections/VehicleRequirementCTA";
 import FaqList from "../../../components/sections/FaqList";
 import BankInfoCTA from "../../../components/sections/BankInfoCTA";
 import FindInfoCTA from "../../../components/sections/FindInfoCTA";
@@ -16,6 +17,9 @@ const RequisitosSocio = ({ data }) => {
   const DrvRequirementColumnsImage = images.filter((image) => {
     return image.title === "mx.DrvRequirementColumns.image";
   });
+  const VehicleRequirementImage = images.filter((image) => {
+    return image.title === "mx.VehicleRequirementCTA.image";
+  })[0];
   const BankInfoCTAImage = images.filter((image) => {
     return image.title === "mx.DiDiPayPhoneWhy.image";
   })[0];
@@ -27,6 +31,9 @@ const RequisitosSocio = ({ data }) => {
       <DrvRequirementColumns
         images={DrvRequirementColumnsImage}
       ></DrvRequirementColumns>
+      <VehicleRequirementCTA
+        image={VehicleRequirementImage}
+      ></VehicleRequirementCTA>
       <BankInfoCTA image={BankInfoCTAImage}></BankInfoCTA>
       <FindInfoCTA></FindInfoCTA>
       <FaqList faqs={faqDrv[0].faq}></FaqList>
@@ -46,6 +53,7 @@ export const query = graphql`
             "mx.RequirementHero.bgImage"
             "mx.DrvRequirementColumns.image"
             "mx.DiDiPayPhoneWhy.image"
+            "mx.VehicleRequirementCTA.image"
           ]
         }
       }
