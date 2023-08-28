464ae2c16a8811a343b4b5de8f410055f778752a
tenisjavier <tenis.javier@gmail.com>
2023-08-28 15:55:33 -0400

    ssl - ab carrousel finished

--- src/pages/cl/index.js
+++ src/pages/cl/index.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { graphql } from "gatsby";
 import Layout from "../../components/Layout";
-import HomeHero from "../../components/sections/HomeHero";
 import HeroCarrousel from "../../components/sections/HeroCarrousel";
 import DrvHero from "../../components/sections/DrvHero";
 import PaxHero from "../../components/sections/PaxHero";
@@ -9,25 +8,13 @@ import SafetyCTA from "../../components/sections/SafetyCTA";
 import DrvCTA from "../../components/sections/DrvCTA";
 import PaxCTA from "../../components/sections/PaxCTA";
 import HomeColumns from "../../components/sections/HomeColumns";
-import { ab } from "../../config/ab";
-import gtmEvent from "../../config/gtm";
 
 const Index = ({ data }) => {
-  const version = ab("2023-07-carrousel-a-t4", "2023-07-carrousel-b-t4", "t4");
   const images = data.allContentfulAsset.nodes;
   const [activeHero, setActiveHero] = useState(0);
   const updateHero = (id) => {
-    gtmEvent(`click-carrousel`, {
-      btnType: "carrousel",
-      versionName: version,
-      tabId: id,
-      countryCode: "cl",
-    });
     setActiveHero(id);
   };
-  const homeHeroBgImage = images.filter((image) => {
-    return image.title === "cl.HomeHero.bgImage";
-  })[0];
   const drvHeroBgImage = images.filter((image) => {
     return image.title === "cl.DrvHero.bgImage";
   })[0];
@@ -59,27 +46,25 @@ const Index = ({ data }) => {
 
   return (
     <Layout sb={false}>
-      {version === "b" && <HomeHero bgImage={homeHeroBgImage}></HomeHero>}
-      {version === "a" && (
-        <>
-          <div className={`${activeHero !== 0 && "hidden"} `}>
-            <DrvHero
-              bgImage={drvHeroBgImage}
-              mobileBgImage={drvHeroMobileBgImage}
-            ></DrvHero>
-          </div>
-          <div className={`${activeHero !== 1 && "hidden"} `}>
-            <PaxHero
-              bgImage={paxHeroBgImage}
-              mobileBgImage={paxHeroMobileBgImage}
-            ></PaxHero>
-          </div>
-          <HeroCarrousel
-            images={carrouselIcons}
-            updateHero={updateHero}
-          ></HeroCarrousel>
-        </>
-      )}
+      <>
+        <div className={`${activeHero !== 0 && "hidden"} `}>
+          <DrvHero
+            bgImage={drvHeroBgImage}
+            mobileBgImage={drvHeroMobileBgImage}
+          ></DrvHero>
+        </div>
+        <div className={`${activeHero !== 1 && "hidden"} `}>
+          <PaxHero
+            bgImage={paxHeroBgImage}
+            mobileBgImage={paxHeroMobileBgImage}
+          ></PaxHero>
+        </div>
+        <HeroCarrousel
+          images={carrouselIcons}
+          updateHero={updateHero}
+        ></HeroCarrousel>
+      </>
+
       <SafetyCTA image={safetyCTAImage}></SafetyCTA>
       <DrvCTA image={drvCTAImage}></DrvCTA>
       <PaxCTA image={paxCTAImage}></PaxCTA>
