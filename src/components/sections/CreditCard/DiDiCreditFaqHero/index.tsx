4fb2709f65ec69e99e0a8ab9615a04ae54c748ff
ArthurBrave <arthur.ropke@brave.ag>
2023-10-04 13:44:54 -0300

    change CreditCardFaqHero to FaqHero in FaqTemplate

--- src/components/sections/CreditCard/DiDiCreditFaqHero/index.tsx
+++ src/components/sections/CreditCard/DiDiCreditFaqHero/index.tsx
@@ -2,31 +2,20 @@ import React from "react";
 import { t } from "../../../../context/countryContext";
 import { ImageDataType } from "../../../Image";
 import CTASection, { CTAProps } from "../../../CTASection";
-import { BtnMode, BtnType } from "../../../Btn";
 
 interface DiDiCreditFaqHeroProps {
   image?: ImageDataType;
-  title?: string;
-  btnType?: BtnType;
-  btnMode?: BtnMode;
-  btnText?: string;
-  bgImage?: ImageDataType;
 }
 
-const DiDiCreditFaqHero = ({ image, title, btnMode, btnText, btnType, bgImage }: DiDiCreditFaqHeroProps) => {
+const DiDiCreditFaqHero = ({ image }: DiDiCreditFaqHeroProps) => {
   const props: CTAProps = {
     hero: true,
-    title: title || t("DiDiCreditFaqHero.title"),
+    title: t("DiDiCreditFaqHero.title"),
     textColor: t("DiDiCreditFaqHero.textColor"),
     bgColor: t("DiDiCreditFaqHero.bgColor"),
-    bgImageStyle: "!absolute z-0 h-full w-full brightness-75 md:block",
-    image,
-    reverse: bgImage ? false : true,
-    whiteRight: bgImage ? false : true,
-    btnMode,
-    btnText,
-    btnType,
-    bgImage,
+    image: image,
+    reverse: true,
+    whiteRight: true,
   };
   return <CTASection {...props}></CTASection>;
 };
