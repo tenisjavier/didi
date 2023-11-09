de66eec776f7bfd6f7f5547a6230c7d567be30e0
Alejandro Poirier <ale.poirier16@gmail.com>
2023-11-09 11:02:50 -0300

    Add Keko Partner for MX

--- src/pages/mx/didimas/index.js
+++ src/pages/mx/didimas/index.js
@@ -40,7 +40,8 @@ const DiDiMas = ({ data }) => {
       partner.name === "Datamovil Arrendadora" ||
       partner.name === "Ventauto" ||
       partner.name === "Mare" ||
-      partner.name === "Procapa Arrendamientos"
+      partner.name === "Procapa Arrendamientos" ||
+      partner.name === "Keko App"
     );
   });
 
